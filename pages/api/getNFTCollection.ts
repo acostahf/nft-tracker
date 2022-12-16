62320ae1690fbf8d89cdfcfd30eb2e5f47465f92
Fabian <acostahf4@gmail.com>
2022-12-16 12:33:49 -0700

    get nft collection

--- pages/api/getNFTCollection.ts
+++ pages/api/getNFTCollection.ts
@@ -2,26 +2,6 @@ import Moralis from "moralis";
 import { EvmChain } from "@moralisweb3/common-evm-utils";
 import type { NextApiRequest, NextApiResponse } from "next";
 
-// const getNFTCollection = async () => {
-// 	await Moralis.start({
-// 		apiKey: process?.env?.MORALIS_KEY,
-// 		// ...and any other configuration
-// 	});
-
-// 	const address = "0xb47e3cd837dDF8e4c57F05d70Ab865de6e193BBB";
-
-// 	const chain = EvmChain.ETHEREUM;
-
-// 	const response = await Moralis.EvmApi.nft.getContractNFTs({
-// 		address,
-// 		chain,
-// 	});
-
-// 	console.log(response.toJSON());
-// };
-
-// getNFTCollection();
-
 type ResponseData = {
 	message: string;
 };
